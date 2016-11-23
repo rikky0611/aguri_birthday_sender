c7808a078e9b3c85b541be1f65e49bc157f2be2b
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-24 01:17:52 +0900

    Refactor iboutlet collection and ibaction func

--- Aguri_Birthday_Sender/ViewController.swift
+++ Aguri_Birthday_Sender/ViewController.swift
@@ -12,7 +12,7 @@ import Firebase
 class ViewController: UIViewController {
 
     let rootRef = FIRDatabase.database().reference()
-    var dict: [String: Bool]!
+    var dict: [Int: Bool]!
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
@@ -22,88 +22,36 @@ class ViewController: UIViewController {
     @IBOutlet var button7: UIButton!
     @IBOutlet var button8: UIButton!
     @IBOutlet var button9: UIButton!
+    
+    @IBOutlet private var buttons: [UIButton]! {
+        
+        didSet {
+            buttons.forEach({
+                
+                $0.backgroundColor = UIColor.red
+            })
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        dict = ["1": false,
-                "2": false,
-                "3": false,
-                "4": false,
-                "5": false,
-                "6": false,
-                "7": false,
-                "8": false,
-                "9": false]
-        button1.backgroundColor = UIColor.red
-        button2.backgroundColor = UIColor.red
-        button3.backgroundColor = UIColor.red
-        button4.backgroundColor = UIColor.red
-        button5.backgroundColor = UIColor.red
-        button6.backgroundColor = UIColor.red
-        button7.backgroundColor = UIColor.red
-        button8.backgroundColor = UIColor.red
-        button9.backgroundColor = UIColor.red
+        for i in 1 ... 9 {
+            
+            dict[i] = false
+        }
         
         update()
     }
     
-    @IBAction func tap1() {
-        dict["1"] = !dict["1"]!
-        changeColor(button: button1)
-        update()
-    }
-    
-    @IBAction func tap2() {
-        dict["2"] = !dict["2"]!
-        changeColor(button: button2)
-        update()
-    }
-    
-    @IBAction func tap3() {
-        dict["3"] = !dict["3"]!
-        changeColor(button: button3)
-        update()
-    }
-    
-    @IBAction func tap4() {
-        dict["4"] = !dict["4"]!
-        changeColor(button: button4)
-        update()
-    }
-    
-    @IBAction func tap5() {
-        dict["5"] = !dict["5"]!
-        changeColor(button: button5)
-        update()
-    }
-    
-    @IBAction func tap6() {
-        dict["6"] = !dict["6"]!
-        changeColor(button: button6)
-        update()
-    }
-    
-    @IBAction func tap7() {
-        dict["7"] = !dict["7"]!
-        changeColor(button: button7)
-        update()
-    }
-    
-    @IBAction func tap8() {
-        dict["8"] = !dict["8"]!
-        changeColor(button: button8)
-        update()
-    }
-    
-    @IBAction func tap9() {
-        dict["9"] = !dict["9"]!
-        changeColor(button: button9)
+    @IBAction private func tap(sender: UIButton) {
+        let index: Int = sender.tag
+        dict[index] = !dict[index]!
+        changeColor(button: buttons[index])
         update()
     }
     
     
-    fileprivate func changeColor(button: UIButton) {
+    private func changeColor(button: UIButton) {
         if button.backgroundColor == UIColor.blue {
             button.backgroundColor = UIColor.red
             return
@@ -114,17 +62,10 @@ class ViewController: UIViewController {
         }
     }
     
-    fileprivate func update() {
-        let post = dict as [String:Bool]
+    private func update() {
+        let post = dict as [Int:Bool]
         rootRef.child("test").setValue(post)
         print("更新完了\(post)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
